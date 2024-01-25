9a9f87d22931af83bbb7e05d1d7bdbd7cf9a3551
Oscar Acuña <89176753+osss2046@users.noreply.github.com>
2024-01-25 00:16:54 -0300

    actualizacion

---
+++ taller4/taller4.py
@@ -0,0 +1,20 @@
+import pandas as pd
+raw_data = pd.read_csv("Star99999_raw.csv")
+raw_data.head(5)
+print(raw_data.head(5))
+
+
+
+###
+
+
+print(type(raw_data))
+
+
+###
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+
---
+++ taller5/taller5.py
@@ -0,0 +1,5 @@
+import pandas as pd
+df = pd.read_csv("urbania_data.csv")
+df.head(5)
+print(df.head())
+
